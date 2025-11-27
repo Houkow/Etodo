be3d8253a22f1f37dce69f32b924c76c4a2aaf48
TheoGarde <theo.garde@epitech.eu>
2025-11-27 11:47:24 +0100

    add docker

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,16 @@
 
 
 import dotenv from 'dotenv';
+dotenv.config();
 import mysql from 'mysql2';
 
-dotenv.config();
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
